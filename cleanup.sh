c24c9ef53ade5fdad9e4bd81af5dadea51978fd6
lucidv01d <clint.cario@gmail.com>
2017-07-19 19:51:43 -0700

    updating download.nf

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "This \033[1;37mWILL DELETE all nextflow run files AND POTENTIALLY other *.log, *.txt, and *.html file\033[0m in this and the work/ directories."
+echo -e "\033[1;37mThis will delete all nextflow run files and potentially other *.log, *.txt, and *.html file in this and the work/ directories.\033[0m"
 echo "This should be OK if you've not modified the workflow/ directory."
 read -r -p "Are you sure? [y/N] " response
 case $response in
